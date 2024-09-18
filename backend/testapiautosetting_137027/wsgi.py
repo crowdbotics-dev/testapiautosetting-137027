dd048ffcc62c0f0d3b7bce0f246020420a81e511
Crowdbotics <engineering@crowdbotics.com>
2024-09-18 07:27:05 +0000

    Initial commit

---
+++ backend/testapiautosetting_137027/wsgi.py
@@ -0,0 +1,18 @@
+"""
+WSGI config for testapiautosetting_137027 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault(
+    "DJANGO_SETTINGS_MODULE", "testapiautosetting_137027.settings"
+)
+
+application = get_wsgi_application()
